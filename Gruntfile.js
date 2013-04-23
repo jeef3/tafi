ffafafe7ae510fccd12a57c5f3ad8edf79f87b42
Jeff Knaggs <mail@jeef3.com>
2013-04-23 22:43:31 +1200

    Swapped qunit for mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,8 +34,8 @@ module.exports = function(grunt) {
         dest: 'dist/<%= pkg.name %>.min.js'
       },
     },
-    qunit: {
-      files: ['test/**/*.html']
+    mocha: {
+      all: ['test/**/*.html']
     },
     jshint: {
       gruntfile: {
@@ -77,9 +77,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
